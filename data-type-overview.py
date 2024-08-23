e8db0a94b8f4fa49b808f43b8c7056abba4bb0c1
SahilShrivastava-Biz <sahilshrivastavabiz@gmail.com>
2024-08-23 15:55:09 +0530

    modularised code commit

--- data-type-overview.py
+++ data-type-overview.py
@@ -1,32 +1,51 @@
-#Checking on the complex numbers
-from math import acosh
+def complex_num():
+    # Checking on the complex numbers
+    x = '1+2j'  # if you give space between the number ,+ sign and imaginary character then it wil throw an error
+    y = complex(x)
+    print(type(x), type(y))
 
-x='1+2j' #if you give space between the number ,+ sign and imaginary character then it wil throw an error
-y= complex(x)
-print(type(x),type(y))
 
-#Testing that the / operator always give output as a float only
-x=12
-y=2
-c=x/y
-print(c,type(c)) #the output type is going to be float only
+
+def div_data_type():
+    # Testing that the / operator always give output as a float only
+    x = 12
+    y = 2
+    c = x / y
+    print(c, type(c))  # the output type is going to be float only
+
+
 
 #using math module for doing operations
 import math
-angle_degrees=60
-angle_radians=math.radians(angle_degrees)
-print(angle_radians)
-print("value of sin x:",math.sin(angle_radians),"\n","value of cos x:",math.cos(angle_radians),"\n","value of tan x:",math.tan(angle_radians))
+def math_class():
+    angle_degrees = 60
+    angle_radians = math.radians(angle_degrees)
+    print(angle_radians)
+    print("value of sin x:", math.sin(angle_radians), "\n", "value of cos x:", math.cos(angle_radians), "\n",
+          "value of tan x:", math.tan(angle_radians))
 
-x=3.4 #interpreted as (x,0)
-y=9.8 #interpreted as (0,y)
-print("Eucledian distance between these two points on their respective axis is ",math.hypot(x,y))
+    x = 3.4  # interpreted as (x,0)
+    y = 9.8  # interpreted as (0,y)
+    print("Eucledian distance between these two points on their respective axis is ", math.hypot(x, y))
+
 
 #Random Library Usage
 import random
-x,y=0,6
-dice_roll=random.randint(x,y)
-print("The digit on the dice will be ",dice_roll)
-rand_num=random.randrange(x,y) # return s a randomly selected number from the sequence
-print(rand_num)
-print(random.random()) #returns a random number between 0 and 1 , is double
+def random_class():
+    x, y = 0, 6
+    dice_roll = random.randint(x, y)
+    print("The digit on the dice will be ", dice_roll)
+    rand_num = random.randrange(x, y)  # return s a randomly selected number from the sequence
+    print(rand_num)
+    print(random.random())  # returns a random number between 0 and 1 , is double
+
+
+#Boolean and comparison operator testing
+def bool_and_comp():
+    a = "1"
+    b = "1"
+    print("is a equals to b", a == b, '\n', 'checking is a IS b ',
+          a is b)  # both conditions holds true , but if you even add an extra space in any of them the object changes , the value doesnt, but the id does (you can check this by print(id(' ')))
+
+    print(a and b)
+
