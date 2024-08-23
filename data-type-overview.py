dd187c3d61efa29d10d426bbdf579c6a44cf065c
SahilShrivastava-Biz <sahilshrivastavabiz@gmail.com>
2024-08-23 14:54:08 +0530

    Test commit for output type of div operator

--- data-type-overview.py
+++ data-type-overview.py
@@ -2,3 +2,9 @@
 x='1+2j' #if you give space between the number ,+ sign and imaginary character then it wil throw an error
 y= complex(x)
 print(type(x),type(y))
+
+#Testing that the / operator always give output as a float only
+x=12
+y=2
+c=x/y
+print(c,type(c))
