8d97933c765f168399863852a17989d8480ca9c1
SahilShrivastava-Biz <sahilshrivastavabiz@gmail.com>
2024-08-26 11:36:52 +0530

    list data type overview committed

--- data-type-overview.py
+++ data-type-overview.py
@@ -1,4 +1,4 @@
-from numpy.ma.extras import ndenumerate
+from asyncio import new_event_loop
 
 
 def complex_num():
@@ -7,6 +7,7 @@ def complex_num():
     y = complex(x)
     print(type(x), type(y))
 
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
 
 
 def div_data_type():
@@ -16,6 +17,7 @@ def div_data_type():
     c = x / y
     print(c, type(c))  # the output type is going to be float only
 
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
 
 
 #using math module for doing operations
@@ -32,6 +34,9 @@ def math_class():
     print("Eucledian distance between these two points on their respective axis is ", math.hypot(x, y))
 
 
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
+
+
 #Random Library Usage
 import random
 def random_class():
@@ -41,6 +46,10 @@ def random_class():
     rand_num = random.randrange(x, y)  # return s a randomly selected number from the sequence
     print(rand_num)
     print(random.random())  # returns a random number between 0 and 1 , is double
+
+
+
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
 
 
 #Boolean and comparison operator testing
@@ -69,7 +78,7 @@ def bool_and_comp():
             pass
         case 'Div':
             pass
-        case "hello world": # Match-case is case sensitive
+        case "hello world": # Match-case is case-sensitive
             print('hello world in caps lock off')
         case "Hello World":
             print("Hello Lock in Caps on")
@@ -83,6 +92,9 @@ def bool_and_comp():
     #Checking the type of None Keyword
     print(type(None))
 
+
+
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
 
 #Checking the operations and basics of loops in python
 def loops():
@@ -110,6 +122,9 @@ def loops():
     else:
         print('Now Big enough to drink, Age:',q)
 
+
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
+
 #testing the properties and operations in string
 def string_ops():
     print("Yes all of them belongs to str class" if type('a')==type("a")==type('''a''')==type("""a""") else "No from different classes")#the type of all these is string only
@@ -136,4 +151,54 @@ def string_ops():
     message=input("Input any 3 words, you will win you can guess it correct:")
     if 'Sahil' or 'friend' or 'bottle' in message:
         print(f"Hurrayy , You guessed the correct word {message}!!")
+
+
+#----------------------------------------------------------------------NEW FUNCTION-----------------------------------------------------------------------------------------------------------------------------------
+
+#checking for operations and functions used in list
+def list_ops():
+    my_list=['hello',32,True,'string',2.928]
+    print('Original list is ',my_list)
+    #change list by index
+    my_list[1]=45
+    print('Updated list is :',my_list)
+    #adding new items in the list
+    my_list.append("Appended object")
+    print("List after using append function :",my_list)
+    #doing slicing and iteration i the list
+    step=2
+    itr_list=my_list[::step]
+    print(itr_list)
+
+    #LIST MANIPULATION
+    int_list=[3,6,1,45,8,432,0,221]
+    print('Integral list is :',int_list)
+    sorted_my_list=sorted(int_list)
+    int_list.sort()
+    print('Integral list after sorting using .sort() keyword as :',int_list)
+    print('Integral list after sorting using "sorted" keyword is as : ',sorted_my_list)
+    print('length of the list is as :',len(int_list))
+    #The extend method allows you to add all the items from another iterable object, for example, from another list or string:
+    some_str='23.45423'
+    my_list.extend(some_str)
+    print(f"my_list after using the extend function and {type(some_str)} {some_str} in between ",my_list)
+    #the count method allows you to count the number of times something appears in the list
+    print(my_list.count('4'))
+    #The pop method allows the removal and returning of items from a list by the given index. If an index is not specified, it removes and returns the last item in a list.
+    print("Last element has been popped out of the list :",my_list.pop(),' \n list after doing the pop operation: ',my_list)
+    idx=5
+    print(f"Element at the {idx} index has been popped from my_list : ",my_list.pop(idx),' \n list after doing the pop operation: ',my_list)
+
+    #using list comprehension
+    n=20
+    even_number=[]
+    for i in range (0,n):
+        if i%2==0 or (not i%2):
+            even_number.append(i)
+    print("List without using list comprehension method:",even_number)
+
+    # new_list = [expression for item in iterable if condition == True]
+    new_even_list=[i for i in range (0,n) if not i%2]
+    print("List using list comprehension method:",new_even_list)
+
 
